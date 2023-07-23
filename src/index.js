742f1f8013eec63c453ea0e2aaf5ed25c0269d30
joelnyongesa <joelnyongesa148@gmail.com>
2023-07-24 01:02:10 +0300

    Complete the project

--- src/index.js
+++ src/index.js
@@ -1,1 +1,20 @@
 // write your code here
+
+// Fetch
+function fetchRamens(){
+    fetch('http://localhost:3000/ramens')
+    .then(response => response.json())
+    .then(data => displayRamens(data))
+    .catch(error => console.log(error))
+}
+// Run the fetch()
+fetchRamens()
+
+
+// Update our DOM
+function displayRamens(ramens){
+    let ramensMenu = document.querySelector('#ramen-menu')
+    for (ramen of ramens){
+        ramensMenu.innerHTML = `<p>ID: ${ramen.id}`
+    }
+}
